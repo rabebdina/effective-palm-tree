cbeb3c7c452d7663eefed89cf6cd2a8655b19397
rabebdina <36415820+rabebdina@users.noreply.github.com>
2019-11-16 10:17:11 +0100

    Update App.java

--- demo/src/main/java/tn/edu/esprit/arctic/App.java
+++ demo/src/main/java/tn/edu/esprit/arctic/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Dina Abid!" );
+        System.out.println( "Bonjour Dina Abid!" );
     }
 }
